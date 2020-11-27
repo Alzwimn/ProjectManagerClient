a480ff0440e866109ca6c248b9b2537eb396938b
Alzwimn <alzwimn@gmail.com>
2020-11-26 23:20:32 -0300

    Htpp method from the browser

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -1,12 +1,22 @@
 import { SessionToken } from "../Models/AuthenticationModels";
 
-
+const baseUrl = "http://localhost:8080"
+const loginUrl = baseUrl + "/login"
 export class LoginService {
 
     public async login(userName: string, password: string): Promise<SessionToken | undefined> {
-        if(userName === "user" && password === "1234") {
-            return { username: "Dummy user"} as any 
+        let options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: userName,
+                password: password
+            })
         }
-        return undefined
+        const result = await fetch(loginUrl, options)
+        if(!result || result.status !== 201) return undefined
+        return await result.json()
     }
 }
