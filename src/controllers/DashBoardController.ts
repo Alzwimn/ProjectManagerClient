34ecf11f932995bb2194541714940b160f8871b2
Alzwimn <alzwimn@gmail.com>
2020-11-28 09:20:47 -0300

    Creating access buttons

--- src/controllers/DashBoardController.ts
+++ src/controllers/DashBoardController.ts
@@ -1,10 +1,12 @@
-import { SessionToken } from "../Models/AuthenticationModels";
+import { AccessRight, SessionToken } from "../Models/AuthenticationModels";
 import { BaseController } from "./BaseController";
 
 
 export class DashBoardController extends BaseController {
 
     private sessionToken: SessionToken | undefined
+    private searchArea: HTMLInputElement | undefined
+    private searchResultArea: HTMLDivElement | undefined
 
     public setSessionToken(sessionToken: SessionToken) {
         this.sessionToken = sessionToken
@@ -16,9 +18,37 @@ export class DashBoardController extends BaseController {
             this.createElement("label", "Please go to the private parts of this Api!")
         } else {
             this.createElement("label", `Welcome ${this.sessionToken?.username}`)
+            this.createElement("br")
+            this.generateButtons()
         }
-        
-
         return this.container
+    }
+
+    private generateButtons() {
+        if(!this.sessionToken) return
+        for(const access of this.sessionToken.accessRight) {
+            this.createElement("button", AccessRight[access], async () => {
+                await this.triggerAction(access)
+            })
+        }
+        if(this.sessionToken.accessRight.includes(AccessRight.READ)) {
+            this.createElement("br")
+            this.createElement("label", "Search: ")
+            this.searchArea = this.createElement("input")
+            this.searchResultArea = this.createElement("div")
+            this.createElement("br")
+        }
+    }
+
+    private async triggerAction(access: AccessRight) {
+        console.log(`Button ${access} clicked!`)
+        switch (access) {
+            case AccessRight.READ:
+                
+                break;
+        
+            default:
+                break;
+        }
     }
 }
